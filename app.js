c8083d3c02f58b727e439fa90191b625803a0499
Ali <actualali.7@gmail.com>
2025-09-28 07:08:08 +0300

    async readFile patterns

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
 const util = require('util');
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile)
 
 const start = async() => {
   try{
-    const data = await getText('./content/first.txt');
-    const second = await getText('./content/second.txt');
-    console.log(data)
+    const first = await readFile('./content/first.txt', 'utf8');
+    const second = await readFile('./content/second.txt', 'utf8');
+    await writeFilePromise('./content/result-mind-grenade.txt', `THIS IS AWESOME: ${first} ${second}`, {flag: 'a'})
+    console.log(first)
     console.log(second)
   }
   catch(err){
     console.log(err)
   }
 }
+
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// const start = async() => {
+//   try{
+//     const data = await getText('./content/first.txt');
+//     const second = await getText('./content/second.txt');
+//     console.log(data)
+//     console.log(second)
+//   }
+//   catch(err){
+//     console.log(err)
+//   }
+// }
 
 start()
