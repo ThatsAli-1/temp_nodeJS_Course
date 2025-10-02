cd2aca88bbe53a7bfdc0282c8942d1484118faa0
Ali <actualali.7@gmail.com>
2025-10-02 07:40:42 +0300

    6:10:00

--- MyFirstFullCode/02-express-tutorial/app.js
+++ MyFirstFullCode/02-express-tutorial/app.js
@@ -1,30 +1,47 @@
-// const app = require('express')()
 const express = require('express');
-
-const app = express();
+const { products } = require('./data.js')
+const app = express(); 
 
 app.get('/', (req, res) => {
-    console.log('user hit the resource');
-    res.status(200).send('Home Page')
+    res.send('<h1> Home Page </h1> <a href="/api/products"> Products </a> ')
 })
 
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page');
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req.params);
+    const { productID } = req.params;
+    const singleProduct = products.find((product) => product.id === Number(productID))
 
+    if (!singleProduct){
+        return res.status(404).send('Product doesn\'t exist. ')
+    }
+    res.json(singleProduct);
 })
 
-app.use((req, res) => {
-    res.status(404).send('Can not be found');
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params);
+    res.send("Hello fucking world");
 })
+
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query);
+    const {search, limit} = req.query
+    let sortedProducts = [...products];
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product) => product.name.startsWith(search))
+    }
+
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit) )
+    }
+    if(!sortedProducts.length){
+        return res.status(200).json({success:true, data:[]})
+    }
+    res.status(200).json(sortedProducts)
+})
+
 
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
+    console.log('Listening on port 5000');
 })
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
